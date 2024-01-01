945396270515a53a0acadb8e7600b074ad5f5f0b
SeyyitBattal <seyyitbattalarvas3@gmail.com>
2024-01-01 19:42:42 +0300

    Project started. Layouts created

--- can_mobilya/src/App.js
+++ can_mobilya/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+//https://dribbble.com/shots/22818650-Carpenter-Craftman-Website
+
+import { Header } from "./Layouts/Header";
+import { Main } from "./Layouts/Main";
+import { Footer } from "./Layouts/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
---
+++ can_mobilya/src/Layouts/Header.js
@@ -0,0 +1,10 @@
+export const Header = () => {
+  return (
+    <div className="">
+      <div className="shadow-2xl">
+        <p>Header Area</p>
+        <p>...</p>
+      </div>
+    </div>
+  );
+};
