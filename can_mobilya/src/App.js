4f81b639e4adacd93544af3944ab83fe21c1f8b7
SeyyitBattal <seyyitbattalarvas3@gmail.com>
2024-01-01 20:11:43 +0300

    App.js updated

--- can_mobilya/src/App.js
+++ can_mobilya/src/App.js
@@ -1,12 +1,26 @@
-//https://dribbble.com/shots/22818650-Carpenter-Craftman-Website
+//TASLAK: https://dribbble.com/shots/22818650-Carpenter-Craftman-Website
+//ESKİ SİTE: https://sites.google.com/view/can-mobilya/ana-sayfa
 
+/*
+TO DO LIST: 
+- Tailwindten headera navigation bar eklenecek
+- 
+*/
+
+import React from "react";
+import { Helmet } from "react-helmet";
 import { Header } from "./Layouts/Header";
 import { Main } from "./Layouts/Main";
 import { Footer } from "./Layouts/Footer";
+import logo from "./Data/can-mobilya-logo.jpg";
 
 function App() {
   return (
     <div className="">
+      <Helmet>
+        <title>Can Mobilya</title>
+        <link rel="icon" href={logo} type="image/jpg" />
+      </Helmet>
       <Header />
       <Main />
       <Footer />
